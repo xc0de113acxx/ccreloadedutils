29757d25152d350d3d4ebf6a4376b4bae0f471a8
xc0de113acxx <77364488+xc0de113acxx@users.noreply.github.com>
2021-01-12 14:54:45 -0800

    Add files via upload

---
+++ src/main/java/com/j1hnt/game/minecraft/mod/item/ItemMod.java
@@ -0,0 +1,34 @@
+package com.j1hnt.game.minecraft.mod.item;
+
+import net.minecraft.item.Item;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+@Mod.EventBusSubscriber
+public class ItemMod {
+
+	// Material
+
+	// Tools
+
+	public static void preInit(){
+
+		// Materials
+
+		// Tools
+
+
+	}
+
+	public static void init(){
+
+		// Register Item Renderers
+
+	}
+
+	@SubscribeEvent
+	public static void registerItems(RegistryEvent.Register<Item> event) {
+		// Register Items
+	}
+}
---
+++ src/main/java/com/j1hnt/game/minecraft/mod/item/ItemRewardToken.java
@@ -0,0 +1,15 @@
+package com.j1hnt.game.minecraft.mod.item;
+
+
+import net.minecraft.creativetab.CreativeTabs;
+
+public class ItemRewardToken extends QuickItem {
+	
+	{
+		name = "Reward Token";
+		tab = CreativeTabs.MISC;
+		texture = "rewardtoken";
+		type = ItemType.HandHeld;
+	}
+	
+}
